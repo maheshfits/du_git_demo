bc721a2899fc0c7373fde99e590ed972f2a0c417
Chathuranga <mahesh.chathuranga@sogeti.com>
2019-10-03 14:02:03 +0200

    add new code

--- JavaApplication/src/javaapplication/JavaApplication.java
+++ JavaApplication/src/javaapplication/JavaApplication.java
@@ -17,6 +17,7 @@ public class JavaApplication {
     public static void main(String[] args) {
         // TODO code application logic here   
         System.out.println("Mahesh");
+        System.out.println("DU");
     }
     
 }
